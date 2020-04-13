1c1b99bd953392ad2dc44fa60cb13a7a95ba2522
PANDA96 <2221395968@qq.com>
2020-04-13 20:01:22 +0800

    Update Solution.java
    最大矩形——添加dp的方法

--- code/MaximalRectangle/Solution.java
+++ code/MaximalRectangle/Solution.java
@@ -1,8 +1,9 @@
 class Solution {
     public int maximalRectangle(char[][] matrix) {
-        return selfMaximalRectangle(matrix);
+        return dpMaximalRectangle(matrix);
     }
 
+    //时间复杂度：N*N*M
     private int selfMaximalRectangle(char[][] matrix) {
         if (matrix.length == 0 || matrix[0].length == 0) {
             return 0;
@@ -26,6 +27,58 @@ class Solution {
                         maxArea = tempArea;
                     }
                 }
+            }
+        }
+
+        return maxArea;
+    }
+
+    //时间复杂度：N*M
+    private int dpMaximalRectangle(char[][] matrix) {
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int curLeft = 0, curRight = n, maxArea = 0;
+
+        int[] height = new int[n];
+        int[] left = new int[n];
+        int[] right = new int[n];
+
+        Arrays.fill(right, n);
+
+        for (int i = 0; i < m; i++) {
+            curLeft = 0;
+            curRight = n;
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == '1') {
+                    height[j]++;
+                } else {
+                    height[j] = 0;
+                }
+            }
+
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == '1') {
+                    left[j] = Math.max(left[j], curLeft);
+                } else {
+                    left[j] = 0;
+                    curLeft = j + 1;
+                }
+            }
+
+            for (int j = n - 1; j >= 0; j--) {
+                if (matrix[i][j] == '1') {
+                    right[j] = Math.min(right[j], curRight);
+                } else {
+                    right[j] = n; 
+                    curRight = j;
+                }
+            }
+
+            for (int j = 0; j < n; j++) {
+                maxArea = Math.max(maxArea, (right[j] - left[j]) * height[j]);
             }
         }
 
