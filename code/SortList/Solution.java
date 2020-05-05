1fb10033b26a755a7375c3c6af30ab391d4dff03
PANDA96 <2221395968@qq.com>
2020-05-05 11:04:08 +0800

    148. 排序链表
    148. 排序链表

---
+++ code/SortList/Solution.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        return selfSortList(head);
+    }
+
+    private ListNode selfSortList(ListNode head) {
+
+        //基本情况
+        if (head == null || head.next == null)
+            return head;
+        
+        //fast = head.next 而不是 fast = head，是为了当链表只有一个元素的时候，能够进行划分
+        ListNode fast = head.next, slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode tmp = slow.next;
+        slow.next = null;
+        ListNode left = sortList(head);
+        ListNode right = sortList(tmp);
+       
+        //合并
+        ListNode h = new ListNode(0);
+        ListNode res = h;
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                h.next = left;
+                left = left.next;
+            } else {
+                h.next = right;
+                right = right.next;
+            }
+            h = h.next;
+        }
+        h.next = left != null ? left : right;
+        return res.next;
+    }
+}
