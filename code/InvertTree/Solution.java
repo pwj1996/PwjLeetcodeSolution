05db623b869d848b7c3eb6c4d7cc5e937ca6084a
PANDA96 <2221395968@qq.com>
2020-05-08 11:09:58 +0800

    226. 翻转二叉树
    226. 翻转二叉树

---
+++ code/InvertTree/Solution.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        selfInvertTree(root);
+        return root;
+    }
+
+    private void selfInvertTree(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        selfInvertTree(root.left);
+        selfInvertTree(root.right);
+    }
+}
